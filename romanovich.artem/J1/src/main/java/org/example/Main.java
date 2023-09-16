9d9508d5b6a95e5eeab9bfc6428b3e38dd9ff245
Artem Romanovich <art.rom.one@gmail.com>
2023-09-16 17:09:45 +0300

    simple work with HashMap

--- romanovich.artem/J1/src/main/java/org/example/Main.java
+++ romanovich.artem/J1/src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("walk", 1);
+        map.put("horse", 2);
+        map.put("fly", 3);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            Integer value = map.get(line);
+            System.out.println(value);
+        }
     }
 }
