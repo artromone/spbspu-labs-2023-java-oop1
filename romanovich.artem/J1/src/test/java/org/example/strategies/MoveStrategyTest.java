66c29299016ff092e22258832398410c9955f39d
Artem Romanovich <art.rom.one@gmail.com>
2023-09-18 13:01:35 +0300

    cleanup

--- romanovich.artem/J1/src/test/java/org/example/strategies/MoveStrategyTest.java
+++ romanovich.artem/J1/src/test/java/org/example/strategies/MoveStrategyTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
 class MoveStrategyTest {
   private CommandProcessor processor;
 
