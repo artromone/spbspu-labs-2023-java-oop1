41288bbeda26ea7400b30e76baed76e56d282d71
Artem Romanovich <art.rom.one@gmail.com>
2023-09-29 21:57:52 +0300

    Successful run

--- romanovich.artem/J2/src/main/java/org/example/Main.java
+++ romanovich.artem/J2/src/main/java/org/example/Main.java
@@ -1,7 +1,6 @@
 package org.example;
 
-import java.util.Objects;
-import org.example.CallNumber;
+import static org.example.MethodKeeper.methodCallCount;
 
 public class Main {
   public static void main(String[] args) throws Exception {
@@ -13,9 +12,17 @@ public class Main {
       CallNumber annotation = method.getAnnotation(CallNumber.class);
       method.setAccessible(true);
 
-      for (int i = 0; i < Objects.requireNonNull(annotation).number(); ++i) {
+      if (annotation == null) {
+        continue;
+      }
+
+      int n = annotation.number();
+      System.out.println("Call times: " + n);
+      for (int i = 0; i < n; ++i) {
+      //for (int i = 0; i < Objects.requireNonNull(annotation).number(); ++i) {
         method.invoke(myClass);
       }
+      methodCallCount = 1;
     }
   }
 }
--- romanovich.artem/J2/src/main/java/org/example/MethodKeeper.java
+++ romanovich.artem/J2/src/main/java/org/example/MethodKeeper.java
@@ -1,32 +1,32 @@
 package org.example;
 
-import org.example.CallNumber;
-
 public class MethodKeeper {
+  public static int methodCallCount = 1;
+
   @CallNumber(number = 2)
   public void publicMethodFirst() {
-    System.out.println("Public method first");
+    System.out.println("Public method first: " + methodCallCount++);
   }
   public void publicMethodSecond() {
-    System.out.println("Public method second");
+    System.out.println("Public method second: " + methodCallCount++);
   }
   public void publicMethodThird() {
-    System.out.println("Public method third");
+    System.out.println("Public method third: " + methodCallCount++);
   }
 
   @CallNumber(number = 3)
   protected void protectedMethodFirst() {
-    System.out.println("Protected method first");
+    System.out.println("Protected method first: " + methodCallCount++);
   }
   protected void protectedMethodSecond() {
-    System.out.println("Protected method second");
+    System.out.println("Protected method second: " + methodCallCount++);
   }
 
   @CallNumber(number = 5)
   private void privateMethodFirst() {
-    System.out.println("Private method first");
+    System.out.println("Private method first: " + methodCallCount++);
   }
   private void privateMethodSecond() {
-    System.out.println("Private method second");
+    System.out.println("Private method second: " + methodCallCount++);
   }
 }
