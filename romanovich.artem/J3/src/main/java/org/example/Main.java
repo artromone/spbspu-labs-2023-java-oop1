ba998369975658cbf2aa4e32c1d83d951fb3d7b8
Artem Romanovich <art.rom.one@gmail.com>
2023-10-05 22:45:17 +0300

    дублирование кода

--- romanovich.artem/J3/src/main/java/org/example/Main.java
+++ romanovich.artem/J3/src/main/java/org/example/Main.java
@@ -3,55 +3,58 @@ package org.example;
 import org.example.animal.Animal;
 import org.example.animal.Carnivore;
 import org.example.animal.Erinaceidae;
+import org.example.animal.Eulipotyphla;
 import org.example.animal.Lynx;
 import org.example.animal.PallasCat;
-import org.example.animal_factories.ErinaceidaeFactory;
-import org.example.animal_factories.LynxFactory;
-import org.example.animal_factories.PallasCatFactory;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 public class Main {
   public static void main(String[] args) {
-    Collection<Animal> animals = new ArrayList<>();
-
-    animals.add(new ErinaceidaeFactory().createAnimal());
-    animals.add(new ErinaceidaeFactory().createAnimal());
-    animals.add(new ErinaceidaeFactory().createAnimal()); // 3
-    animals.add(new PallasCatFactory().createAnimal());
-    animals.add(new PallasCatFactory().createAnimal());   // 2
-    animals.add(new LynxFactory().createAnimal());        // 1
-
-    Collection<? super Erinaceidae> erinaceidaes = new ArrayList<>();
-    Collection<? super Carnivore> pallasCats = new ArrayList<>();
-    Collection<? super Carnivore> lynxes = new ArrayList<>();
-
-    segregate(animals, erinaceidaes, pallasCats, lynxes);
-
-    System.out.println(erinaceidaes.size());
-    System.out.println(pallasCats.size());
-    System.out.println(lynxes.size());
+//    Collection<Animal> animals = new ArrayList<>();
+//
+//    animals.add(new ErinaceidaeFactory().createAnimal());
+//    animals.add(new ErinaceidaeFactory().createAnimal());
+//    animals.add(new ErinaceidaeFactory().createAnimal()); // 3
+//    animals.add(new PallasCatFactory().createAnimal());
+//    animals.add(new PallasCatFactory().createAnimal());   // 2
+//    animals.add(new LynxFactory().createAnimal());        // 1
+//
+//    Collection<? super Erinaceidae> erinaceidaes = new ArrayList<>();
+//    Collection<? super Carnivore> pallasCats = new ArrayList<>();
+//    Collection<? super Carnivore> lynxes = new ArrayList<>();
+//
+//    segregate(animals, erinaceidaes, pallasCats, lynxes);
+//
+//    System.out.println(erinaceidaes.size());
+//    System.out.println(pallasCats.size());
+//    System.out.println(lynxes.size());
   }
 
-  public static void segregate(Collection<? extends Animal> src,
-                               Collection<? super Erinaceidae> erinaceidaes,
-                               Collection<? super Carnivore> pallasCats,
-                               Collection<? super Carnivore> lynxes) {
+  public static <
+      T extends Eulipotyphla,
+      U extends Carnivore,
+      V extends Carnivore
+      > void segregate(Collection<? extends Animal> src,
+                       Collection<? super T> erinaceidaes,
+                       Collection<? super U> pallasCats,
+                       Collection<? super V> lynxes) {
 
     for (Animal animal : src) {
       if (animal == null) {
         continue;
       }
 
+      System.out.println(animal.getClass());
+
       if (animal instanceof Erinaceidae) {
-        erinaceidaes.add((Erinaceidae) animal);
+        erinaceidaes.add((T) animal);
       }
       if (animal instanceof PallasCat) {
-        pallasCats.add((PallasCat) animal);
+        pallasCats.add((U) animal);
       }
       if (animal instanceof Lynx) {
-        lynxes.add((Lynx) animal);
+        lynxes.add((V) animal);
       }
     }
   }
--- romanovich.artem/J3/src/test/java/org/example/segregate/SegregateTest.java
+++ romanovich.artem/J3/src/test/java/org/example/segregate/SegregateTest.java
@@ -3,8 +3,11 @@ package org.example.segregate;
 import static org.example.Main.segregate;
 
 import org.example.animal.Carnivore;
+import org.example.animal.Chordate;
 import org.example.animal.Erinaceidae;
+import org.example.animal.Felidae;
 import org.example.animal.Mammal;
+import org.example.animal.PallasCat;
 import org.example.animal_factories.AnimalFactory;
 import org.example.animal_factories.EuropeanHedgehogFactory;
 import org.example.animal_factories.LynxFactory;
@@ -15,38 +18,76 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class SegregateTest {
 
   @Test
   @DisplayName("Test 'Млекопитающие, Ежовые, Кошачьи, Хищные'")
-  public void testSegregate() {
+  public void testSegregateFirst() {
     List<Mammal> animals = new ArrayList<>();
 
-    int[] animalCounts = {3, 2, 1};
-    AnimalFactory[] factories = {
-        new EuropeanHedgehogFactory(),
-        new PallasCatFactory(),
-        new LynxFactory()
-    };
+    Map<AnimalFactory, Integer> animalRequests = new HashMap<>();
+    animalRequests.put(new EuropeanHedgehogFactory(), 3);
+    animalRequests.put(new PallasCatFactory(), 2);
+    animalRequests.put(new LynxFactory(), 1);
 
-    for (int i = 0; i < factories.length; i++) {
-      AnimalFactory factory = factories[i];
-      int count = animalCounts[i];
+    for (Map.Entry<AnimalFactory, Integer> entry : animalRequests.entrySet()) {
+      AnimalFactory factory = entry.getKey();
+      Integer count = entry.getValue();
 
       for (int j = 0; j < count; j++) {
         animals.add((Mammal) factory.createAnimal());
       }
     }
 
-    Collection<? super Erinaceidae> erinaceidaes = new ArrayList<>();
-    Collection<? super Carnivore> pallasCats = new ArrayList<>();
-    Collection<? super Carnivore> lynxes = new ArrayList<>();
+    Collection<Erinaceidae> erinaceidaes = new ArrayList<>();
+    Collection<Felidae> pallasCats = new ArrayList<>();
+    Collection<Carnivore> lynxes = new ArrayList<>();
 
     segregate(animals, erinaceidaes, pallasCats, lynxes);
 
     Assertions.assertEquals(3, erinaceidaes.size());
+    Assertions.assertEquals(2, pallasCats.size());
+    Assertions.assertEquals(1, lynxes.size());
+
+    for (Object erinaceidae : erinaceidaes) {
+      Assertions.assertNotNull(erinaceidae);
+    }
+    for (Object pallasCat : pallasCats) {
+      Assertions.assertNotNull(pallasCat);
+    }
+    for (Object lynx : lynxes) {
+      Assertions.assertNotNull(lynx);
+    }
+  }
+
+  @Test
+  @DisplayName("Test 'Хищные, Хордовые, Манулы, Кошачьи'")
+  public void testSegregateSecond() {
+    List<Carnivore> animals = new ArrayList<>();
+
+    Map<AnimalFactory, Integer> animalRequests = new HashMap<>();
+    animalRequests.put(new PallasCatFactory(), 2);
+    animalRequests.put(new LynxFactory(), 1);
+
+    for (Map.Entry<AnimalFactory, Integer> entry : animalRequests.entrySet()) {
+      AnimalFactory factory = entry.getKey();
+      Integer count = entry.getValue();
+
+      for (int j = 0; j < count; j++) {
+        animals.add((Carnivore) factory.createAnimal());
+      }
+    }
+
+    Collection<Chordate> erinaceidaes = new ArrayList<>();
+    Collection<PallasCat> pallasCats = new ArrayList<>();
+    Collection<Felidae> lynxes = new ArrayList<>();
+
+    segregate(animals, erinaceidaes, pallasCats, lynxes);
+
     Assertions.assertEquals(2, pallasCats.size());
     Assertions.assertEquals(1, lynxes.size());
 
