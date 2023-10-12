f10309d876e270b666d92651be5caf82f1824d7b
Artem Romanovich <art.rom.one@gmail.com>
2023-10-13 00:02:24 +0300

    Lab done

--- romanovich.artem/J4/src/main/java/org/example/FileParser.java
+++ romanovich.artem/J4/src/main/java/org/example/FileParser.java
@@ -23,7 +23,6 @@ public class FileParser {
     } catch (IOException e) {
       Logger.error(e.getMessage());
     }
-
     return dictionary;
   }
 
@@ -34,5 +33,56 @@ public class FileParser {
       String translation = parts[1];
       dictionary.put(word, translation);
     }
+  }
+
+  public String translate(String inputFileName) throws Exception {
+    return findTranslationImpl(inputFileName);
+  }
+
+  private void formatWord(String word) {
+    word = word.trim().toLowerCase();
+  }
+
+  private void formatTranslation(String translation) {
+    translation = translation.trim();
+  }
+
+  private String findTranslationImpl(String inputFileName) throws Exception {
+    StringBuilder translation = new StringBuilder();
+
+    try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName))) {
+      String line;
+
+      while ((line = reader.readLine()) != null) {
+        String[] words = line.split("\\s+");
+
+        for (String word : words) {
+          translation.append(translateImpl(word.toLowerCase())).append(" ");
+        }
+
+        translation.append("\n");
+      }
+    } catch (Exception e) {
+      throw new Exception("Input file not found: " + inputFileName);
+    }
+    return translation.toString();
+  }
+
+  private String translateImpl(String word) {
+    if (dictionary.containsKey(word)) {
+      return dictionary.get(word);
+    }
+
+    String bestMatch = "";
+    for (String key : dictionary.keySet()) {
+      if (word.startsWith(key) && key.length() > bestMatch.length()) {
+        bestMatch = key;
+      }
+    }
+
+    if (!bestMatch.isEmpty()) {
+      return dictionary.get(bestMatch);
+    }
+    return word;
   }
 }
